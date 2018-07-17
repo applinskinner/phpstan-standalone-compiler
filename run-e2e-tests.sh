2f1380928468f1a5a47912b59c3f5ebeaa491bd4
Lukáš Unger <looky.msc@gmail.com>
2018-07-17 13:12:12 +0200

    E2E tests

---
+++ run-e2e-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# compile phar
+if [ ! -f tmp/phpstan.phar ]; then
+    php bin/compile
+fi
+
+# setup
+cd e2e
+rm -rf vendor
+rm -f composer.lock
+composer install --no-interaction
+cp -f ../tmp/phpstan.phar vendor/phpstan/phpstan-shim/phpstan.phar
+cp -f ../tmp/phpstan.phar vendor/phpstan/phpstan-shim/phpstan
+
+# test that the phar autoloader works
+php testPharAutoloader.php
+
+# test levels
+vendor/bin/phpunit PharTest.php
